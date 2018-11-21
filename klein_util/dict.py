b7882b39916821495eebd0f1a8b8df85ed2aaaee
Matt Cockayne <matthew.cockayne@md.catapult.org.uk>
2018-11-21 08:01:39 +0000

    corrects bad serialization

--- klein_util/dict.py
+++ klein_util/dict.py
@@ -7,5 +7,5 @@ def traverse_dict(data, parts):
     if key not in data:
         raise LookupError(
             "Key '%s' does not exist in %s" %
-            (key, json.dumps(data)))
+            (key, json.dumps(list(data.keys()))))
     return traverse_dict(data[key], parts) if remaining > 1 else data[key]
