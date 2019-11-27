a8edcb3037672e42d293d680b0cdfb98f0e07231
daniel.james <daniel.james@md.catapult.org.uk>
2019-11-27 16:31:51 +0000

    Adds `get_document_with_ner` function plus docs & test.

--- src/klein_util/doclib.py
+++ src/klein_util/doclib.py
@@ -81,3 +81,34 @@ def get_metadata_index_by_value(metadata, value):
     :return:
     """
     return _get_metadata_index_helper(metadata, 'value', value)
+
+
+def get_document_with_ner(
+        doc_query,
+        doc_collection,
+        ner_collection_name="ner"
+):
+    """
+    Return a matching document with added NER information.
+
+    Only returns a single document, so only suitable for use with queries that
+    are expected to return a single hit - e.g. "_id" or other unique field.
+
+    :param dict doc_query: a dict defining the document
+    :param pymongo.collection.Collection doc_collection: the document mongo
+    collection object
+    :param str ner_collection_name: the ner mongo collection name (default:
+    "ner")
+    :return: the new document as dict
+    """
+    # from pdb import set_trace
+    # set_trace()
+    return doc_collection.aggregate([
+        {"$match": doc_query},
+        {"$lookup": {
+            "from": ner_collection_name,
+            "localField": "_id",
+            "foreignField": "document",
+            "as": "ner"}
+        }
+    ]).next()
