7d790e2f8b0bc6c870fef7ca335952e2f9102ae6
Matthew Hodgskiss <matthew.hodgskiss@md.catapult.org.uk>
2020-03-06 14:40:12 +0000

    Increment version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 
 setup(name='klein_util',
-      version='0.1.12',
+      version='0.1.13',
       description='Utility Functions & Classes',
       url='http://gitlab.mdcatapult.io/informatics/klein/klein_util',
       author='Matt Cockayne',
