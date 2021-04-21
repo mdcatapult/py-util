a8e5d3e8986a0d0de1d1ea5e3f3497d662d5e927
Mark Laing <mark.laing@md.catapult.org.uk>
2021-04-21 18:58:17 +0100

    Makes notify function compatible with latest py-config.

--- src/klein_util/notifications.py
+++ src/klein_util/notifications.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 import json
-from klein_config import config
+from klein_config import get_config
 import boto3
 
 
 def notify(msg, **kwargs):
+    config = get_config()
     if config.has("notifications.topic") and config.has("aws"):
         msg_string = json.dumps(msg)
         client = boto3.client('sns', region_name=config.get("aws.region"))
